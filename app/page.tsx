eb73bf89c92ba9b35982100796b7d029b5f45b4a
Tricky <trickya23@outlook.com>
2024-11-17 16:15:02 +0000

    addprojects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import styles from "./index.module.css"
 import { Hero } from "@/components/hero";
 import { About } from "@/components/about";
+import { Projects } from "@/components/projects";
 
 export default function Page() {
   return (
     <main className={styles.main}>
       <Hero />
       <About />
+      <Projects />
     </main>
   )
 }
